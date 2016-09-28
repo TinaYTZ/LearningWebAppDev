874215c14cf9f0059cfc83f34f9c597697545d3c
Yuting Zhang <tinaytz@users.noreply.github.com>
2016-09-27 18:10:13 -0700

    comments odd even

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -5,21 +5,34 @@ var main = function () {
     var addCommentFromInputBoxA = function () {
         if ($('.comment-inputA input').val() !=='') {
             var $newMessage= document.createElement('p');
-            $newMessage.setAttribute('class', 'chat_A');
+            if(a%2!=0)
+            {
+              $newMessage.setAttribute('class', 'chat_A_Odd');
+              
+            }
+            else{ $newMessage.setAttribute('class', 'chat_A_Even');}
             $newMessage.innerHTML= $('.comment-inputA input').val();
             $('.comments').append($newMessage);
             $('.comment-inputA input').val('');
+            a=a+1;
         }
+
     };
 
 
     var addCommentFromInputBoxB = function () {
         if ($('.comment-inputB input').val() !== '') {
             var $newMessage= document.createElement('p');
-            $newMessage.setAttribute('class', 'chat_B');
+            if(a%2!=0)
+            {
+              $newMessage.setAttribute('class', 'chat_B_Odd');
+              
+            }
+            else{ $newMessage.setAttribute('class', 'chat_B_Even');}
             $newMessage.innerHTML= $('.comment-inputB input').val();
             $('.comments').append($newMessage);
             $('.comment-inputB input').val('');
+            b=b+1;
         }
     };
 
@@ -43,5 +56,7 @@ var main = function () {
         }
     });
 };
+var a=1;
+var b=1;
 
 $(document).ready(main);
